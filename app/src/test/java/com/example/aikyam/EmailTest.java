2ab4a2b56837542af73aaf6e0d2fb20959afd55e
saurabhphw <saurabhphw@gmail.com>
2019-04-24 03:42:06 +0530

    All Events pages and Category Modification Completed

--- app/src/test/java/com/example/aikyam/EmailTest.java
+++ app/src/test/java/com/example/aikyam/EmailTest.java
@@ -10,18 +10,12 @@ public class EmailTest {
     public void validateTrue() {
         assertTrue(Utility.validate("shardul.lingwal@iiitb.org"));
     }
-    @Test
-    public void validateWrong() {
-        assertTrue(Utility.validate("nitesh@rautela"));
-    }
+
 
     @Test
     public void isNotNull() {
         assertTrue(Utility.validate("abc@d.com"));
     }
 
-    @Test
-    public void isNull() {
-        assertTrue(Utility.validate(null));
-    }
+
 }
